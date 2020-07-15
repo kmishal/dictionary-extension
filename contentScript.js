e7e53e6a7c85f51610d66de26031012ed169f13a
kaustubh <mishal.kaustubh@gmail.com>
2020-07-16 03:46:32 +0530

    dictionary prototype done

--- contentScript.js
+++ contentScript.js
@@ -1,77 +1,134 @@
-const iFrame = document.createElement("iframe");
-iFrame.src = chrome.extension.getURL("dictionary-ui.html");
+let dicContainer;
 
 let variableHelpers = {
-	// apiUrl: chrome.runtime.getURL("dictionary.json"),
-	apiUrl: "https://api.dictionaryapi.dev/api/v1/entries/en/",
-	getSelectedTerm: "",
-	iframePos: {
-		top: null,
-		right: null,
-	},
+    apiUrl: 'https://api.dictionaryapi.dev/api/v1/entries/en/',
+    getSelectedTerm: '',
+    iframePos: {
+        top: null,
+        right: null,
+    },
+    subData: [],
 };
 
 // Get the term when user dbclicks on a text
-document.addEventListener("dblclick", function (e) {
-	variableHelpers.getSelectedTerm = window
-		.getSelection()
-		.toString()
-		.toLowerCase();
-	getMousePos(e);
-	getMeaning(variableHelpers.getSelectedTerm);
+document.addEventListener('dblclick', function (e) {
+    variableHelpers.getSelectedTerm = window
+        .getSelection()
+        .toString()
+        .toLowerCase();
+    getMousePos(e);
+    getMeaning(variableHelpers.getSelectedTerm);
 });
 
 const getMousePos = function (mouseEvent) {
-	let mousePosTop = mouseEvent.clientX;
-	let mousePosLeft = mouseEvent.clientY;
-	console.log(mouseEvent);
-	variableHelpers.iframePos = {
-		top: mousePosTop,
-		left: mousePosLeft,
-	};
+    let mousePosTop = mouseEvent.clientX;
+    let mousePosLeft = mouseEvent.clientY;
+    console.log(mouseEvent);
+    variableHelpers.iframePos = {
+        top: mousePosTop,
+        left: mousePosLeft,
+    };
 };
 
-const generateUIForSunKey = function (keyData) {
-	const {iframePos} = variableHelpers;
-	const {subKeyName, subKeyData} = keyData;
+const generateFinalUIBody = function (layout) {
+    let html = `<div class="o_box">
+            <div class="close_pop">X Close</div>
+            <div class="o_title"></div>
+            <div class="co_cont"></div>
+		</div>`;
 
-	console.log("/========================");
-	console.log(subKeyName);
-	for (let i = 0; i < subKeyData.length; i++) {
-		console.log(subKeyData[i].definition);
-	}
-	console.log("/========================");
+    let parentdiv = document.createElement('div');
+    let closeBtn = document.createElement('div');
+    let title = document.createElement('p');
+    title.classList.add('o_title');
+    closeBtn.classList.add('close_pop');
+    title.textContent = variableHelpers.getSelectedTerm;
+    closeBtn.textContent = 'X Close';
+    parentdiv.classList.add('o_box_wrap');
+    parentdiv.appendChild(title);
+    parentdiv.appendChild(closeBtn);
+    parentdiv.appendChild(layout);
+    document.body.insertBefore(parentdiv, document.body.firstChild);
+    document.querySelector('.close_pop').addEventListener('click', function () {
+        dicContainer = document.querySelectorAll('.o_box_wrap');
+        console.log(dicContainer);
+        removeContainer();
+    });
+};
 
-	iFrame.style.top = `${iframePos.top}px`;
-	iFrame.style.left = `${iframePos.right}px`;
-	document.body.insertBefore(iFrame, document.body.firstChild);
+const storeDefinationLayout = function (layout) {
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('o_box');
+    for (let i = 0; i < layout.length; i++) {
+        cardBody.appendChild(layout[i]);
+    }
+    generateFinalUIBody(cardBody);
+};
+
+const generateUIForSunKey = function (keyData, callBack) {
+    const { iframePos } = variableHelpers;
+    const { subKeyName, subKeyData } = keyData;
+    let wordDefinationLayout = document.createElement('div');
+    wordDefinationLayout.classList.add('sub-text');
+    // create subkeyEL
+    let h4El = document.createElement('h4');
+    let ulEl = document.createElement('ul');
+    h4El.textContent = subKeyName;
+    wordDefinationLayout.appendChild(h4El);
+    wordDefinationLayout.appendChild(ulEl);
+    console.log(subKeyName);
+    for (let i = 0; i < subKeyData.length; i++) {
+        let createParaTag = document.createElement('li');
+        createParaTag.textContent = subKeyData[i].definition;
+        ulEl.appendChild(createParaTag);
+    }
+    console.log('/========================');
+    variableHelpers.subData.push(wordDefinationLayout);
+    console.log(variableHelpers);
 };
 
 // Search for the term in retunr json file
-const searchMeaning = function (dataObj) {
-	const {searchTerm, jsonData} = dataObj;
-	// My all word segregation logic comes here!!
-	console.log(jsonData);
-	Object.keys(jsonData).map((key) => {
-		// "meaning" obj key has all my definations
-		let jsonKey = jsonData[key];
-		if (key == "meaning") {
-			//check for array data-type
-			Object.keys(jsonKey).map((subKey) => {
-				if (Array.isArray(jsonKey[subKey])) {
-					generateUIForSunKey({
-						subKeyName: subKey,
-						subKeyData: jsonKey[subKey],
-					});
-				}
-			});
-		}
-	});
+const searchMeaning = function (dataObj, callBack) {
+    const { searchTerm, jsonData } = dataObj;
+    // My all word segregation logic comes here!!
+    console.log(jsonData);
+    Object.keys(jsonData).map((key) => {
+        // "meaning" obj key has all my definations
+        let jsonKey = jsonData[key];
+        if (key == 'meaning') {
+            //check for array data-type
+            Object.keys(jsonKey).map((subKey) => {
+                if (Array.isArray(jsonKey[subKey])) {
+                    generateUIForSunKey({
+                        subKeyName: subKey,
+                        subKeyData: jsonKey[subKey],
+                    });
+                }
+            });
+        }
+    });
+    console.log(variableHelpers.subData);
+    storeDefinationLayout(variableHelpers.subData);
 };
 
 // search for word in dictionary
 const getMeaning = function (word) {
-	fetch(`${variableHelpers.apiUrl}${word}`)
-		.then((response) => response.json())
-		.then((data) => searchMeaning({searchTerm: word, jsonData: data[0]}));
+    fetch(`${variableHelpers.apiUrl}${word}`)
+        .then((response) => response.json())
+        .then((data) => searchMeaning({ searchTerm: word, jsonData: data[0] }));
+};
+
+const removeContainer = function () {
+    try {
+        if (dicContainer.length > -1) {
+            for (let i = 0; i < dicContainer.length; i++) {
+                dicContainer[i].remove();
+                console.log(dicContainer);
+            }
+            variableHelpers.subData = [];
+            variableHelpers.searchTerm = '';
+        }
+    } catch (err) {
+        console.log(err, 'cannot find container');
+    }
 };
